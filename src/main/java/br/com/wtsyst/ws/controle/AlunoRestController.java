b48ce57549445b583fbaa90747dba589dac104b1
UiltonAlves <uilnew18@gmail.com>
2018-02-27 22:45:39 -0300

    atualização na liberação do aluno no webserveci

--- src/main/java/br/com/wtsyst/ws/controle/AlunoRestController.java
+++ src/main/java/br/com/wtsyst/ws/controle/AlunoRestController.java
@@ -3,11 +3,7 @@ package br.com.wtsyst.ws.controle;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.wtsyst.bean.Aluno;
@@ -23,12 +19,6 @@ public class AlunoRestController {
 	@GetMapping("/consultar_alunos")
 	public List<Aluno> getCustomers() {
 		return controle.select();
-	}
-
-	@PostMapping(value = "/cadastrar_aluno")
-	public ResponseEntity createCustomer(@RequestBody Aluno aluno) {
-		this.controle.insert(aluno);
-		return new ResponseEntity(aluno, HttpStatus.OK);
 	}
 
 }
