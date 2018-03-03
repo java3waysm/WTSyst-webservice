141899c428aa9ea85cd9d3f1f11bd87874e4c441
UiltonAlves <uilnew18@gmail.com>
2018-03-03 10:28:04 -0300

    cadastro de Aluno. webserver

--- src/main/java/br/com/wtsyst/ws/controle/AlunoRestController.java
+++ src/main/java/br/com/wtsyst/ws/controle/AlunoRestController.java
@@ -3,8 +3,13 @@ package br.com.wtsyst.ws.controle;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import br.com.wtsyst.bean.Aluno;
 import br.com.wtsyst.controle.AlunoBCI;
@@ -20,5 +25,11 @@ public class AlunoRestController {
 	public List<Aluno> getCustomers() {
 		return controle.select();
 	}
-
+	
+	@PostMapping(value = "/cadastrar_alunos")
+	public ResponseEntity cadastrarAlunos(@RequestBody Aluno aluno) {
+		this.controle.insert(aluno);
+		
+		return new ResponseEntity(aluno, HttpStatus.OK);
+	}
 }
